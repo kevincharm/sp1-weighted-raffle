2a52ac6df5d564c9e07246f0da019f4133e9127c
Kevin Charm <kevin@kevincharm.com>
2024-07-31 15:32:04 +0200

    fix draw test

--- program/src/raffle/feistel.rs
+++ program/src/raffle/feistel.rs
@@ -2,7 +2,9 @@ use sha3::{Digest, Keccak256};
 
 // Babylonian sqrt
 fn sqrt(s: u64) -> u64 {
+    #[cfg(not(test))]
     println!("cycle-tracker-start: sqrt");
+
     let mut z = 0;
     if s > 3 {
         z = s;
@@ -14,24 +16,31 @@ fn sqrt(s: u64) -> u64 {
     } else if s != 0 {
         z = 1;
     }
+
+    #[cfg(not(test))]
     println!("cycle-tracker-end: sqrt");
     z
 }
 
 // Take next perfect square unless n is already one
 fn next_perfect_square(n: u64) -> u64 {
+    #[cfg(not(test))]
     println!("cycle-tracker-start: next-perfect-square");
     let sqrt_n = sqrt(n);
     if sqrt_n.pow(2) == n {
         return n;
     }
     let out = (sqrt_n + 1).pow(2);
+
+    #[cfg(not(test))]
     println!("cycle-tracker-end: next-perfect-square");
     out
 }
 
 fn f(x: u64, i: u64, seed: u64, modulus: u64) -> u64 {
+    #[cfg(not(test))]
     println!("cycle-tracker-start: round-func");
+
     let mut keccak = Keccak256::new();
     keccak.update(x.to_le_bytes());
     keccak.update(i.to_le_bytes());
@@ -40,6 +49,8 @@ fn f(x: u64, i: u64, seed: u64, modulus: u64) -> u64 {
     let output: [u8; 32] = keccak.finalize().into(); // 64b
     let trunc_output = output[24..32].try_into().unwrap();
     let out = u64::from_be_bytes(trunc_output);
+
+    #[cfg(not(test))]
     println!("cycle-tracker-end: round-func");
     out
 }
@@ -50,6 +61,7 @@ pub fn shuffle(_x: u64, seed: u64, domain: u64, rounds: u64) -> u64 {
     assert!(_x < domain, "x too large");
     assert!((rounds & 1) == 0, "rounds must be even");
 
+    #[cfg(not(test))]
     println!("cycle-tracker-start: shuffle");
     let mut x = _x;
     let h = sqrt(next_perfect_square(domain));
@@ -66,6 +78,8 @@ pub fn shuffle(_x: u64, seed: u64, domain: u64, rounds: u64) -> u64 {
             break;
         }
     }
+
+    #[cfg(not(test))]
     println!("cycle-tracker-end: shuffle");
     x
 }
@@ -73,9 +87,10 @@ pub fn shuffle(_x: u64, seed: u64, domain: u64, rounds: u64) -> u64 {
 #[allow(dead_code)]
 pub fn deshuffle(_x_prime: u64, seed: u64, domain: u64, rounds: u64) -> u64 {
     assert!(domain != 0, "modulus must be > 0");
-    assert!(_x_prime < domain, "x too large");
+    assert!(_x_prime < domain, "x too large {_x_prime} >= {domain}");
     assert!((rounds & 1) == 0, "rounds must be even");
 
+    #[cfg(not(test))]
     println!("cycle-tracker-start: deshuffle");
     let mut x_prime = _x_prime;
     let h = sqrt(next_perfect_square(domain));
@@ -92,6 +107,8 @@ pub fn deshuffle(_x_prime: u64, seed: u64, domain: u64, rounds: u64) -> u64 {
             break;
         }
     }
+
+    #[cfg(not(test))]
     println!("cycle-tracker-end: deshuffle");
     x_prime
 }
--- program/src/raffle/mod.rs
+++ program/src/raffle/mod.rs
@@ -4,7 +4,7 @@ use feistel::deshuffle;
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Entry {
     pub address: [u8; 20],
     pub start: u64,
@@ -34,22 +34,67 @@ fn compute_winner(n: u64, entries: &[Entry], seed: [u8; 32]) -> [u8; 20] {
 }
 
 pub fn draw(num_winners: u64, seed: [u8; 32], entries: &[Entry]) -> Vec<[u8; 20]> {
+    assert!(num_winners > 0, "num_winners == 0");
     assert!(
         num_winners <= entries.len() as u64,
         "num_winners > |entries|"
     );
+
     let mut winners = HashSet::new();
     let mut i = 0u64;
     for _ in 0..num_winners {
-        let mut winner;
         loop {
-            winner = compute_winner(i, entries, seed);
+            let winner = compute_winner(i, entries, seed);
             i += 1;
             if !winners.contains(&winner) {
+                winners.insert(winner);
                 break;
             }
         }
-        winners.insert(winner);
     }
     winners.into_iter().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::{self, Rng};
+    use std::collections::HashSet;
+
+    #[test]
+    fn test_draw() {
+        let num_entries = 1000;
+        let mut entries = vec![];
+        for i in 0..num_entries {
+            entries.push(Entry {
+                address: rand::thread_rng().r#gen(),
+                start: i * 10,
+                end: i * 10 + 10,
+            });
+        }
+        assert_eq!(
+            entries.len(),
+            num_entries as usize,
+            "rng did not generate {num_entries} distinct entries (rerun the test!)"
+        );
+        let num_winners = num_entries;
+        let seed = rand::thread_rng().r#gen();
+        let winners = draw(num_winners, seed, &entries);
+        assert_eq!(
+            winners.len(),
+            num_winners as usize,
+            "wrong number of winners drawn"
+        );
+
+        // Check winners are distinct
+        let mut winners_set = HashSet::new();
+        for winner in &winners {
+            winners_set.insert(winner);
+        }
+        assert_eq!(
+            winners_set.len(),
+            num_winners as usize,
+            "winners are not distinct"
+        );
+    }
 }
